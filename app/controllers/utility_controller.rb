632d3183e5009f43cffd4c3a96b3e39ff78533f7
Neeraj Kumar <neeraj.kumar@w3villa.com>
2015-05-10 19:32:15 +0530

    Fixes in utility controller

--- app/controllers/utility_controller.rb
+++ app/controllers/utility_controller.rb
@@ -19,10 +19,11 @@ class UtilityController < ApplicationController
 		REDIS_CLIENT.SADD("club_configs", "club_config:#{club_config.id}")
 			REDIS_CLIENT.HMSET("club_config:#{club_config.id}", "name", club_config.name, "club_type", club_config.club_type);		
 			club_config.clubs.each do |club|
+				p club
 			  @club_type =  ClubConfig.where(id:  Club.where(id: club.id).pluck(:club_config_id)).pluck(:club_type)
 				REDIS_CLIENT.SADD("clubs","club:#{club.id}")
 				REDIS_CLIENT.SADD("club_config_clubs:#{club.club_config_id}", "club:#{club.id}")
-				REDIS_CLIENT.ZADD("club_config_occupancy:#{club.club_config_id}", 2, "club:#{club.id}")
+				REDIS_CLIENT.ZADD("club_config_occupancy:#{club.club_config_id}", 0, "club:#{club.id}")
 				REDIS_CLIENT.HMSET("club:#{club.id}", "name", club.name, "club_config_id", club.club_config_id, "entry_fees",  club.entry_fees, "club_type", @club_type, "winner_amount", club.winner_amount)
 			end
 		end
