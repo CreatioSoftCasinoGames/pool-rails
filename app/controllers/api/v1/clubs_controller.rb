3b0b782bdac9c67c41f68c40799f884a98fc0b30
Neeraj Kumar <amrendra.singh@w3villa.com>
2015-08-06 19:42:06 +0530

    gift

--- app/controllers/api/v1/clubs_controller.rb
+++ app/controllers/api/v1/clubs_controller.rb
@@ -8,13 +8,15 @@ class Api::V1::ClubsController < Api::V1::ApplicationController
 
 	def create
 		@club = Club.new(club_config_id: params[:club_config_id])
+		@club_type = ClubConfig.where(id: params[:club_config_id]).first.club_type
 
 		if @club.save
 			render json: {
 				club: @club.as_json({
 					only: [:id, :name, :entry_fees, :winner_amount, :club_config_id]
 					}),
-				valid: true
+				valid: true,
+				club_type: @club_type
 			}
 		else
 			render json: {
